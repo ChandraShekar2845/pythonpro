7d810daa904cc40e49e1382b7af29ca6f5f08a68
chandhu <chandhu@gmail.com>
2025-01-25 14:17:24 +0530

    v3

--- mainGUI.py
+++ mainGUI.py
@@ -7,7 +7,7 @@ from PIL import ImageTk, Image
 
 from predictions import predict
 
-# global variables for bone 
+# global variables for bone facture detection 
 
 project_folder = os.path.dirname(os.path.abspath(__file__))
 folder_path = project_folder + '/images/'
